56b3202555ecddf89c4ca90ed6c9dbfdad9d7b8e
stevenzab <steven.zabandith@epitech.eu>
2026-03-11 23:26:44 +0100

    [FIX] remove hidden for lg

--- app/components/appshell.tsx
+++ app/components/appshell.tsx
@@ -17,10 +17,9 @@ export default function AppShell({ children }: AppShellProps) {
                 openSideMenu={openSideMenu}
                 onToggleMenu={() => setOpenSideMenu((prev) => !prev)}
             />
-
             <div className="flex">
                 {openSideMenu && (
-                    <aside className="w-64 min-h-[calc(100vh-61px)] border-r border-gray-200/50 bg-white p-5 lg:hidden">
+                    <aside className="w-64 min-h-[calc(100vh-61px)] border-r border-gray-200/50 bg-white p-5">
                         <SideMenu onClose={() => setOpenSideMenu(false)} />
                     </aside>
                 )}
