5841d2067e757c103dd17befaa1a95e3cf1d9b6f
stevenzab <steven.zabandith@epitech.eu>
2026-03-11 23:20:42 +0100

    [FIX] use link home when click on title

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,27 @@
 "use client";
-import { useState } from "react";
+import Link from "next/link";
 import { IoMdMenu } from "react-icons/io";
 
-export default function Navbar() {
-    const [openSideMenu, setOpenSideMenu] = useState(false);
+type NavbarProps = {
+    onToggleMenu: () => void;
+    openSideMenu: boolean;
+};
+
+export default function Navbar({ onToggleMenu, openSideMenu }: NavbarProps) {
 
     return (
-        <div className="flex gap-5 bg-white border border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
+        <div className="relative flex gap-5 bg-white border border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
             <IoMdMenu
                 className="text-2xl text-gray-700 cursor-pointer lg:hidden"
+                onClick={onToggleMenu}
             />
-            <h2 className="text-lg font-medium text-black">Financio</h2>
+            <Link
+                href="/"
+                className="text-lg font-medium text-black"
+            >
+                Financio
+            </Link>
         </div>
+
     );
 }
