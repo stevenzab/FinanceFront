a892144f4a3796f6444d45996220b0054a1a3178
stevenzab <steven.zabandith@epitech.eu>
2026-04-07 22:35:17 +0200

    [DELETE] remove unused openSideMenu prop from Navbar component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,23 +4,23 @@ import { IoMdMenu } from "react-icons/io";
 
 type NavbarProps = {
     onToggleMenu: () => void;
-    openSideMenu: boolean;
 };
 
-export default function Navbar({ onToggleMenu, openSideMenu }: NavbarProps) {
-
+export default function Navbar({ onToggleMenu }: NavbarProps) {
     return (
-        <div className="relative flex gap-5 bg-white border border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
-            <IoMdMenu
-                className="text-2xl text-gray-700 cursor-pointer lg:hidden"
-                onClick={onToggleMenu}
-            />
-            <Link
-                href="/dashboard"
-                className="text-lg font-medium text-black"
-            >
-                Financio
-            </Link>
+        <div className="relative flex items-center justify-between gap-5 bg-white border border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
+            <div className="flex items-center gap-5">
+                <IoMdMenu
+                    className="text-2xl text-gray-700 cursor-pointer lg:hidden"
+                    onClick={onToggleMenu}
+                />
+                <Link
+                    href="/dashboard"
+                    className="text-lg font-medium text-black"
+                >
+                    Financio
+                </Link>
+            </div>
         </div>
 
     );
